258e1c8c8f32ac4301d95c492105d98c7865f6dc
bitzblack <158268598+bitzblack@users.noreply.github.com>
2024-06-08 03:26:13 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'b0cb1b0b-f5ba-4ca4-a587-cfe48126bff7';
 
-const พร็อกซีไอพีs = ['89.34.227.133'];
+const พร็อกซีไอพีs = [''];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
